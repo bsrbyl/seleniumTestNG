914a4db8997bfd0794681f3038313117269d12df
Busra Boyali <bsrbyl.bus@gmail.com>
2022-05-31 23:47:02 +0300

    31.05.22

--- src/test/java/practice/Q2_DependsOn.java
+++ src/test/java/practice/Q2_DependsOn.java
@@ -17,5 +17,16 @@ public class Q2_DependsOn extends TestBase {
 
  */
 
-
+    @Test
+    public void facebookTest(){
+        driver.get("http://www.facebook.com");
+    }
+    @Test(dependsOnMethods="facebookTest")
+    public void googleTest(){
+        driver.get("http://www.google.com");
+    }
+    @Test(dependsOnMethods="googleTest")
+    public void amazonTest(){
+        driver.get("http://www.amazon.com");
+    }
 }
--- src/test/java/practice/Q3_SoftAssert.java
+++ src/test/java/practice/Q3_SoftAssert.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
+import pages.SauceDemoPage;
+import utilities.Driver;
 import utilities.TestBase;
 
 import java.util.ArrayList;
@@ -27,7 +29,42 @@ public class Q3_SoftAssert extends TestBase {
 
 
     // Verify item prices are sorted from low to high with hard Assert
+    @Test
+    public void sauceDemoSoft(){
+        Driver.getDriver().get("https://www.saucedemo.com/");
+        SauceDemoPage sdpage = new SauceDemoPage();
+        sdpage.username.sendKeys("standard_user");
+        sdpage.password.sendKeys("secret_sauce");
+        sdpage.loginButton.click();
+        Select select =new Select(sdpage.dropDown);
+        select.selectByVisibleText("Price (low to high)");
+        String expected = "PRICE (LOW TO HIGH)";
+        String actual = select.getFirstSelectedOption().getText();
+        String actual2 = Driver.getDriver().findElement(By.className("active_option")).getText();
+        SoftAssert softAssert=new SoftAssert();
+        softAssert.assertEquals(actual,expected);
+        softAssert.assertEquals(actual2,expected);
+    }
+    @Test
+    public void sauceDemoHard(){
+        Driver.getDriver().get("https://www.saucedemo.com/");
+        SauceDemoPage sdpage = new SauceDemoPage();
+        sdpage.username.sendKeys("standard_user");
+        sdpage.password.sendKeys("secret_sauce");
+        sdpage.loginButton.click();
+        Select select =new Select(sdpage.dropDown);
+        select.selectByIndex(2);
+        ArrayList<Double> urunlerDouble = new ArrayList<>();
+        for (WebElement each: sdpage.urunler){
+            //String fiyatStr = each.getText().replaceAll("$", "");
+            String fiyatStr = each.getText().replaceAll("^\\D", "");
+            urunlerDouble.add(Double.parseDouble(fiyatStr));
+        }
 
+        ArrayList<Double> kontrolListe = new ArrayList<>(urunlerDouble);
+        Collections.sort(kontrolListe);
+        Assert.assertEquals(kontrolListe,urunlerDouble);
+    }
 
 
 }
